ff11d81144610a8986cea7c4fc8bf67cbb081630
Yuriy Lipchey <xaosmmm@gmail.com>
2021-02-26 13:38:49 +0200

    Added main functions

--- Greetr.js
+++ Greetr.js
@@ -1,15 +1,65 @@
-const jQuery = require('./jquery-3.5.1');
-
+//const jQuery = require('./jquery-3.5.1');
+//import $ from './jquery-3.5.1'
 
 (function (global, $) {
 
-    let Greetr = function (firstName, lastName, language){
+    let Greetr = function (firstName, lastName, language) {
         return new Greetr.init(firstName, lastName, language)
     }
+    let supportedLang = ['en', 'es', 'ru']
 
-    Greetr.prototype = {}
+    let greetings = {
+        en: 'Hello',
+        es: 'Hola',
+        ru: 'Привет'
+    }
 
-    Greetr.init = function (firstName, lastName, language){
+    let formalGreeetings = {
+        en: 'Greetings',
+        es: 'Saludos',
+        ru: 'Здравствуйте'
+    }
+
+    let logMessages = {
+        en: 'Logged in',
+        es: 'Inicio sesion',
+        ru: 'Залогинено'
+    }
+
+
+    Greetr.prototype = {
+        fullName: function () {
+            return this.firstName + ' ' + this.lastName
+        },
+
+        validate: function () {
+            if (!supportedLang.includes(this.language)) throw 'Invalid language'
+        },
+
+        greeting: function () {
+            return greetings[this.language] + ' ' + this.firstName + '!'
+        }
+        formalGreeetings: function () {
+            return formalGreeetings[this.language] + ', ' + this.fullName()
+        }
+
+        greet: function (formal) {
+            let msg
+            if (formal) {
+                msg = this.formalGreeetings()
+            } else {
+                msg = this.greeting()
+            }
+            if (console) {
+                console.log(msg)
+            }
+            //'this' refers to the calling object at execution time
+            // makes the method chainable
+            return this
+        }
+    }
+
+    Greetr.init = function (firstName, lastName, language) {
         let self = this
         self.firstName = firstName || ''
         self.lastName = lastName || ''
@@ -18,9 +68,10 @@ const jQuery = require('./jquery-3.5.1');
     }
     Greetr.init.prototype = Greetr.prototype
 
-    //create aliases to Greetr in globl object
-    global.Greetr = global.G$  = Greetr
+    //create aliases to Greetr in global object
+    global.Greetr = global.G$ = Greetr
 
 
+}(window, $))
 
-}(window, jQuery))
+
